17d1f83e80b1e3877e376a940d2600d668f08e0c
Furkan Emin Can <femincanx@gmail.com>
2023-05-30 19:37:11 +0300

    feat(styling): extend theme with chakra-ui-prose

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,22 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { withProse } from '@nikolovlazar/chakra-ui-prose';
 
 const config: ThemeConfig = {
   initialColorMode: 'system',
   useSystemColorMode: true,
 };
 
-const theme = extendTheme({
-  fonts: {
-    heading: "'IBM Plex Sans', sans-serif",
-    body: "'IBM Plex Sans', sans-serif",
-    mono: "'IBM Plex Mono', monospace",
-    logo: "'Cherry Bomb One', cursive",
+const theme = extendTheme(
+  {
+    fonts: {
+      heading: "'IBM Plex Sans', sans-serif",
+      body: "'IBM Plex Sans', sans-serif",
+      mono: "'IBM Plex Mono', monospace",
+      logo: "'Cherry Bomb One', cursive",
+    },
+    config,
   },
-  config,
-});
+  withProse()
+);
 
 export { theme };
