341c0a312e7288b73db8977094141a80d763eb88
Furkan Emin Can <femincanx@gmail.com>
2023-05-30 09:56:02 +0300

    refactor: use normal export for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Box } from '@chakra-ui/react';
 
 const App = () => (
-  <Box bg="blue.200" w="100vw" maxW="full" h="100vh">
+  <Box bg='blue.200' w='100vw' maxW='full' h='100vh'>
     App
   </Box>
 );
-export default App;
+export { App };
