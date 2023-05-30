6e1041a4be893a640e8e5c4f6ebca0a18a6615a0
Furkan Emin Can <femincanx@gmail.com>
2023-05-30 15:32:31 +0300

    feat(components): add Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+import { Flex, Link, Text } from '@chakra-ui/react';
+
+const Footer = () => (
+  <Flex
+    sx={{
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+      fontSize: 'large',
+      gap: 1,
+
+      '& a': {
+        textDecoration: 'underline',
+      },
+    }}
+  >
+    <Text>
+      Made with 💖 by{' '}
+      <Link href='https://github.com/femincan'>Furkan Emin Can</Link>
+    </Text>
+    <Text>
+      Click <Link href='https://github.com/femincan/turbo-md'>here</Link> to
+      access project&apos;s GitHub repository
+    </Text>
+  </Flex>
+);
+
+export { Footer };
