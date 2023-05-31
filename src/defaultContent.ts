8cc5c2c33a29988165cd3000452e4093cc7f5b1c
Furkan Emin Can <femincanx@gmail.com>
2023-05-31 17:12:06 +0300

    feat: add defaultContent constant

---
+++ src/defaultContent.ts
@@ -0,0 +1,32 @@
+const defaultContent = `# Front-End Development Newsletter
+
+## React Server Components
+
+\`React Server Components\` are both new and impressive.
+
+**Here is an example of using React Server Components:**
+
+\`\`\`javascript
+const MyServerComponent = async () => {
+  const post = await fetchPost('https://example.com/posts');
+
+  return <div>{post}</div>;
+};
+\`\`\`
+
+## List of Front-End Libraries That I Like Most
+
+- [React](https://react.dev)
+- [Vue](https://vuejs.org)
+- [Astro](https://astro.build)
+- [SvelteKit](https://kit.svelte.dev)
+
+## A True Story
+
+![react meme](https://b1694534.smushcdn.com/1694534/wp-content/uploads/2021/06/12-1.png)
+
+## Quote of the Day
+
+> The best preparation for tomorrow is doing your best today. (H. Jackson Brown, Jr.)`;
+
+export { defaultContent };
